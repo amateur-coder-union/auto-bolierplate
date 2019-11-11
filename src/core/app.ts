d61e2179b7c408936adc41bc1ec3fc5b996986b1
lear <1325925707@qq.com>
2019-11-12 00:10:50 +0800

    fix: add example

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,12 +1,19 @@
 // import { delay } from '../utils';
-// import dx from '../../assets/1.png';
+import img from '../../test.png';
 
 class App {
   start = async () => {
-    toast('hello!');
+    toast(`hello, ${device.fingerprint}`);
+    console.show();
+    sleep(1200); // autojs提供的sleep函数，同步delay.
+    // await delay(1200); // 自己写的delay函数，利用promise，建议还是用框架提供的
+    const point = images.findColor(img, '#a4ca39');
+    console.log(point);
+    console.log(device.serial);
+    console.log(device.sdkInt);
+    console.verbose(device.release);
   };
 }
 
 const app = new App();
-
 export default app;
